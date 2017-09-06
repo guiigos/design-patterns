dfaf41598a5374929cfca4a8a322524f8ac4da73
Guilherme Alves <guilherme.alves@insidesistemas.com.br>
2017-09-06 09:13:57 -0300

    Remoção de importações não utilizadas

--- DesignPatterns/TemplateMethod/Imposto.cs
+++ DesignPatterns/TemplateMethod/Imposto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace TemplateMethod
+﻿namespace TemplateMethod
 {
     public interface Imposto
     {
